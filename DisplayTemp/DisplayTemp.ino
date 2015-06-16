73eded64b7402efed649dbb5a5cd31487bac49bf
rheingoldheavy <dan@rheingoldheavy.com>
2015-06-16 14:21:15 -0700

    Modified constants and fixed bug calling init_AT30TS750

--- DisplayTemp/DisplayTemp.ino
+++ DisplayTemp/DisplayTemp.ino
@@ -10,25 +10,25 @@
 #include <I2C.h>
 
 // Temperature Sensor I2C address and registers
-const byte  AT30TS750_I2C           = 0x48;  // Temperature Sensor IC I2C Address
-const byte  REGISTER_TEMP           = 0x00;  // Register Address: temperature data
-const byte  REG_CONFIG              = 0x01;  // Register Address: Sensor Configuration
+const int  AT30TS750_I2C           = 0x48;  // Temperature Sensor IC I2C Address
+const int  REGISTER_TEMP           = 0x00;  // Register Address: temperature data
+const int  REG_CONFIG              = 0x01;  // Register Address: Sensor Configuration
 
 // Display Driver I2C address and registers
-const byte  AS1115_I2C              = 0x00; // Display Manager IC I2C Address
-const byte  REGISTER_SHUTDOWN_MODE  = 0x0C; // Register Address: Display Shutdown
-const byte  REGISTER_DECODE         = 0x09; // Register Address: Digit decode / no decode
-const byte  REGISTER_SCAN_LIMIT     = 0x0B; // Register Address: Digit enable
-const byte  REGISTER_INTENSITY      = 0x0A; // Register Address: Global brightness
+const int  AS1115_I2C              = 0x00; // Display Manager IC I2C Address
+const int  REGISTER_SHUTDOWN_MODE  = 0x0C; // Register Address: Display Shutdown
+const int  REGISTER_DECODE         = 0x09; // Register Address: Digit decode / no decode
+const int  REGISTER_SCAN_LIMIT     = 0x0B; // Register Address: Digit enable
+const int  REGISTER_INTENSITY      = 0x0A; // Register Address: Global brightness
 
-const byte  DIGIT_0                 = 0x01; // Left most seven segment digit
-const byte  DIGIT_1                 = 0x02; // Second seven segment digit
-const byte  DIGIT_2                 = 0x03; // Third seven segment digit
-const byte  DIGIT_3                 = 0x04; // Right most seven segment digit
-const byte  BTM_RED                 = 0x05; // Bottom Red bar graph multiplexed with L1/L2/L3
-const byte  BTM_GRN                 = 0x06; // Bottom Green bar graph
-const byte  TOP_RED                 = 0x07; // Top Red bar graph
-const byte  TOP_GRN                 = 0x08; // Top Green bar graph
+const int  DIGIT_0                 = 0x01; // Left most seven segment digit
+const int  DIGIT_1                 = 0x02; // Second seven segment digit
+const int  DIGIT_2                 = 0x03; // Third seven segment digit
+const int  DIGIT_3                 = 0x04; // Right most seven segment digit
+const int  BTM_RED                 = 0x05; // Bottom Red bar graph multiplexed with L1/L2/L3
+const int  BTM_GRN                 = 0x06; // Bottom Green bar graph
+const int  TOP_RED                 = 0x07; // Top Red bar graph
+const int  TOP_GRN                 = 0x08; // Top Green bar graph
 
 long        errorCount              = 0;    // A variable for holding how many errors occur
 byte        errorStatus             = 0;    // A variable to hold the I2C command error code
@@ -48,7 +48,7 @@ void setup()
 
   Serial.begin(9600);
 
-  init_AT30TS750;
+  init_AT30TS750();
   init_AS1115();
   enableDegreeSymbol (true);
   
